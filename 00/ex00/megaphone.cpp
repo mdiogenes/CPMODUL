19f9925d73116e1e70956e53d33899cf75ce5869
msoler-e <mdiogenes@gmail.com>
2023-01-17 12:13:24 +0100

    primer exdercici

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msoler-e <msoler-e@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 11:56:07 by msoler-e          #+#    #+#             */
+/*   Updated: 2023/01/17 11:56:50 by msoler-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int
+	main(int argc, char const *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(str[j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
